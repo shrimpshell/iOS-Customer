41514ded6ed2549edcc22e97146ba899238a4ebe
黃信 <tedhwang8@hotmail.com>
2018-10-05 10:27:24 +0800

    first commit

---
+++ iOS-Customer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Customer
+//
+//  Created by Hsin Hwang on 2018/10/4.
+//  Copyright © 2018 Hsin Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

78bbbc6c01504a27eb5282fe8b627bbd95492830
UnaCP102 <xu3bji4vup326@gmail.com>
2018-11-04 14:32:28 +0800

    no message

--- iOS-Customer/Model/Customer.swift
+++ iOS-Customer/Model/Customer.swift
@@ -18,7 +18,7 @@ class  Customer {    //不確定要用struct還是用class
     var birthday: String
     var phone: String
     var address: String
-    var customerPic: Int    //此處屬性要修改
+    var customerPic: Int? = nil   //此處屬性要修改
 
 
     init (idCustomer: Int, customerID: String, name: String, email: String, password: String, gender: String, birthday: String, phone: String, address: String, customerPic: Int) {
@@ -31,19 +31,7 @@ class  Customer {    //不確定要用struct還是用class
         self.birthday = birthday
         self.phone = phone
         self.address = address
-        self.customerPic = customerPic
     }
     
-    init (idCustomer: Int, customerID: String, name: String, email: String, password: String, gender: String, birthday: String, phone: String, address: String) {
-        self.idCustomer = idCustomer
-        self.customerID = customerID
-        self.name = name
-        self.email = email
-        self.password = password
-        self.gender = gender
-        self.birthday = birthday
-        self.phone = phone
-        self.address = address
-    }
-        
+
 }

0a0478bd7451600db9ef14a9c82d22c89112a241
una <xu3bji4vup326@gmail.com>
2018-12-07 13:52:07 +0800

    2018-12-07 更

--- iOS-Customer/iOS-Customer/Model/Common.swift
+++ iOS-Customer/iOS-Customer/Model/Common.swift
@@ -12,10 +12,10 @@ import Foundation
 struct Common {
 
     // Server
-    static let SERVER_URL: String = "http://192.168.50.46:8080/ShellService"
+    static let SERVER_URL: String = "http://192.168.50.105:8080/ShellService"
 
     // Websocket
-    static let SOCKET_URL: String = "ws://192.168.50.46:8080/ShellService/WsServer/"
+    static let SOCKET_URL: String = "ws://192.168.50.105:8080/ShellService/WsServer/"
 }
 
 

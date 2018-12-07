56893dbab310fad253bdb0b23ba3cee5f63f7fd2
una <xu3bji4vup326@gmail.com>
2018-12-07 15:47:31 +0800

    2018-12-07 二更

--- iOS-Customer/iOS-Customer/Control/HomePage/AllRatingsTableViewCell.swift
+++ iOS-Customer/iOS-Customer/Control/HomePage/AllRatingsTableViewCell.swift
@@ -58,7 +58,7 @@ class AllRatingsTableViewCell: UITableViewCell {
             }
             
             customrServiceResponse.isHidden = false
-            if allRating?.ratingStatus != 2 {
+            if allRating?.ratingStatus == 1 {
                 customrServiceResponse.isHidden = true
             }
         }

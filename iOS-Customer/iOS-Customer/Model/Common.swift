fdc606e7bab6566a05b68ec1b809f383c56fe1ca
Hsun <tony760815@gmail.com>
2018-12-14 13:59:51 +0800

    修正marge問題

--- iOS-Customer/iOS-Customer/Model/Common.swift
+++ iOS-Customer/iOS-Customer/Model/Common.swift
@@ -12,22 +12,12 @@ import Foundation
 struct Common {
 
     // Server
-<<<<<<< HEAD
     static let SERVER_URL: String = "http://192.168.50.188:8080/ShellService"
 //    static let SERVER_URL: String = "http://192.168.1.101:8080/ShellService"
 
     // Websocket
     static let SOCKET_URL: String = "ws://192.168.50.188:8080/ShellService/WsServer/"
 //    static let SOCKET_URL: String = "ws://192.168.1.101:8080/ShellService/WsServer/"
-   
-=======
-    static let SERVER_URL: String = "http://192.168.50.46:8080/ShellService"
-//    static let SERVER_URL: String = "http://192.168.1.101:8080/ShellService"
-
-    // Websocket
-    static let SOCKET_URL: String = "ws://192.168.50.46:8080/ShellService/WsServer/"
-//    static let SOCKET_URL: String = "ws://192.168.1.101:8080/ShellService/WsServer/"
->>>>>>> develop
 }
 
 

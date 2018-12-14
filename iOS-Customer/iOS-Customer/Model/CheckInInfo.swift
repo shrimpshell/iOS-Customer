79993b7527dbc40bcba789703bc2333f494ff982
una <xu3bji4vup326@gmail.com>
2018-12-14 14:08:19 +0800

    fags

---
+++ iOS-Customer/iOS-Customer/Model/CheckInInfo.swift
@@ -0,0 +1,17 @@
+//
+//  CheckInInfo.swift
+//  iOS-Customer
+//
+//  Created by Una Lee on 2018/12/14.
+//  Copyright © 2018 Hsin Hwang. All rights reserved.
+//
+
+import Foundation
+
+struct Customer: Codable {
+    var idCustomer: Int? = nil
+    var customerID: String? = nil
+    var name: String
+    var email: String
+    
+}

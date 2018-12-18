4e49305a89317d2089f207ee97006bf0156b2e64
una <xu3bji4vup326@gmail.com>
2018-12-18 10:09:44 +0800

    2018-12-18 update
    修正登入判斷式

--- iOS-Customer/iOS-Customer/Control/ProfilePage/ProfileViewController.swift
+++ iOS-Customer/iOS-Customer/Control/ProfilePage/ProfileViewController.swift
@@ -62,12 +62,16 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     override func viewWillAppear(_ animated: Bool) {
         super .viewWillAppear(true)
+        guard let idCustomer = userID.object(forKey: "userID") else {
+            print("idCustomer 解包錯誤")
+            return
+        }
         self.tabBarController?.tabBar.isHidden = false
         hideKeyboard()
         checkInTitleLabel.isHidden = true
         checkInfomation.isHidden = true
         if ProfileViewController.isLogin == true {
-            showCustomerInfo(idCustomer: self.idCustomer)
+            showCustomerInfo(idCustomer: idCustomer as! Int)
         }
     }
     
@@ -89,12 +93,13 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         let customerValid =  ["action": "userValid", "email": email, "password": password] as [String : String]
         customerTask.isCorrectUser(customerValid).done { (idCustomer) in
-            let id = Int(idCustomer)
-            self.userID.set(id, forKey: "userID")
-            guard idCustomer.count > 0 || idCustomer != "0" || (Int(idCustomer) != nil)  else {
+            
+            guard idCustomer != "0" else {
                 self.showAlert(message: "帳號或密碼不正確，請重新輸入")
                 return
             }
+            let id = Int(idCustomer)
+            self.userID.set(id, forKey: "userID")
             self.idCustomer = Int(idCustomer)!
             self.idCustomerLabel.text = "\(idCustomer)"
             DispatchQueue.main.async {
@@ -105,6 +110,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
     }
     
+    //SHOW~!!! CustomerInformation~!
     func showCustomerInfo(idCustomer: Int) {
         let customerTask = CustomerAuth()
         let orderDetails = OrderRoomDB()
@@ -131,29 +137,6 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             self.nameCustomer.text = customer.name
             self.emailCustomer.text = customer.email
             self.phoneCustomer.text = customer.phone
-            let parameters = ["action":"getRoomPayDetailById", "idCustomer":"\(self.idCustomer)"]
-            return orderDetails.getRoomPayDetailById(parameters)
-        }.then { rooms -> Promise<[OrderInstantDetail]> in
-            self.orderRoomDetails = rooms
-            let parameters = ["action":"getInstantPayDetail", "idCustomer":"\(self.idCustomer)"]
-            return orderDetails.getInstantPayDetail(parameters)
-        }.then {
-            instants -> Promise<Data?> in
-            self.orderInstantDetails = instants
-            ProfileViewController.isLogin = true
-            let getCustomerImage: [String : Any] = ["action": "getImage", "IdCustomer": idCustomer]
-            return customerTask.getCustomerImage(getCustomerImage)
-        }.done {
-            data in
-            if (data?.count)! > 0 {
-                DispatchQueue.main.async() {
-                    self.imageCustomer.image = UIImage(data: data!)
-                }
-            } else {
-                self.imageCustomer.image = UIImage(named: "picture")
-                self.imageCustomer.backgroundColor = .white
-            }
-            
             self.customerAuth.getUserRoomReservationStatus(idCustomer: idCustomer as! Int) {
                 (result, error) in
                 if let error = error {
@@ -215,6 +198,32 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
                     self.serviceBtn.isEnabled = false
                 }
             }
+            let parameters = ["action":"getRoomPayDetailById", "idCustomer":"\(idCustomer)"]
+            return orderDetails.getRoomPayDetailById(parameters)
+        }.then { rooms -> Promise<[OrderInstantDetail]> in
+            self.orderRoomDetails = rooms
+            print("orderRoomDetails: \(self.orderRoomDetails!)")
+            let parameters = ["action":"getInstantPayDetail", "idCustomer":"\(idCustomer)"]
+            return orderDetails.getInstantPayDetail(parameters)
+        }.then {
+            instants -> Promise<Data?> in
+            self.orderInstantDetails = instants
+            print("orderInstantDetails: \(self.orderInstantDetails!)")
+            ProfileViewController.isLogin = true
+            let getCustomerImage: [String : Any] = ["action": "getImage", "IdCustomer": idCustomer]
+            return customerTask.getCustomerImage(getCustomerImage)
+        }.done {
+            data in
+            if !(data?.isEmpty)! {
+                DispatchQueue.main.async() {
+                    self.imageCustomer.image = UIImage(data: data!)
+                }
+            } else {
+                self.imageCustomer.image = UIImage(named: "picture")
+                self.imageCustomer.backgroundColor = .white
+            }
+            
+           
         }.catch { (error) in
             assertionFailure("Login Error: \(error)")
         }
@@ -378,6 +387,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
                     assertionFailure("originalImage is nil.")
                     return
             }
+            //照片上傳
             let resizedImage = originalImage.resize(maxEdge: 1024)!
             let jpgData = resizedImage.jpegData(compressionQuality: 0.8)
             //let pngData = resizedImage.pngData()
@@ -406,6 +416,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         isFromCheckBooking = false
         checkInTitleLabel.isHidden = true
         checkInfomation.isHidden = true
+        self.userID.set("", forKey: "roomNumber")
         self.userID.set(0, forKey: "userID")
         self.userID.synchronize()
         imageCustomer.image = UIImage(named: "")
@@ -413,10 +424,12 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         print("Log Out")
     }
     
+    //去
     @IBAction func toOrderListPageButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: "toOrderList", sender: nil)
     }
     
+    //帶資料
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let customer = self.customer else {
             return
@@ -425,9 +438,11 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         case "toOrderList":
             let NAVController = segue.destination as? UINavigationController
             let tableViewController = NAVController?.viewControllers.first as! RoomOrderTableViewController
+            print("orderRoomDetails: \(self.orderRoomDetails!), orderInstantDetails: \(self.orderInstantDetails!)")
             tableViewController.orders = self.orderRoomDetails!
             tableViewController.instants = self.orderInstantDetails!
             tableViewController.idCustomer = self.idCustomer
+            
         case "toRatingList":
             let NAVController = segue.destination as? UINavigationController
             let ratingListPage = NAVController?.viewControllers.first as! AllRatingsTableViewController
